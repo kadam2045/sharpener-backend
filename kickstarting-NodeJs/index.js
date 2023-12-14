bcc7ce3208d0501b88a0b26e88378971e88c920e
Shubham Kadam <shubham.kadam@appgambit.com>
2023-12-14 23:02:04 +0530

    create server

--- kickstarting-NodeJs/index.js
+++ kickstarting-NodeJs/index.js
@@ -1,1 +1,8 @@
-console.log("hello word");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("server started sucessfully", req);
+  res.end("shubham kadam  ");
+});
+
+server.listen(3000);
