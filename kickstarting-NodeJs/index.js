8156cd6b5b2fc870a86a899711ba4c044c53ce25
Shubham Kadam <shubham.kadam@appgambit.com>
2024-01-04 00:11:16 +0530

    base on url custome response

--- kickstarting-NodeJs/index.js
+++ kickstarting-NodeJs/index.js
@@ -1,8 +1,20 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("server started sucessfully", req);
-  res.end("shubham kadam  ");
+  const url = req.url;
+
+  if (url === "/home") {
+    res.write("welcome to home");
+    return res.end();
+  }
+  console.log("server started sucessfully", req.url, req.method);
+  res.setHeader("Content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>my page</title></head>");
+  res.write("<body><h2>Welcome to my Nodejs project</h2></body>");
+  res.write("</html>");
+
+  res.end();
 });
 
 server.listen(3000);
