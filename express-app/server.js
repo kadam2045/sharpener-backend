508cefbbfb8f9596b764afea2bcd0e89efae2ac5
Shubham Kadam <shubham.kadam@appgambit.com>
2024-01-15 16:57:51 +0530

    add two input & parse body

--- express-app/server.js
+++ express-app/server.js
@@ -1,16 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 const port = 4000;
 
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  console.log("in the product middlware");
+
+  res.send(
+    '<form action="/product" method="POST"> <input type="text" name="title"><input type="number" name="count"> <button type="submit">Add Product</button></form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("in the another middlware");
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>hellow</h1>");
 });
+
 app.listen(port);
